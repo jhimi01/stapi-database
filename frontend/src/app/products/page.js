471ef742d466a3f6f22348c38c0a2d6ea4e485f7
jhimi01 <talukderjhimi@gmail.com>
2024-09-08 23:02:32 +0600

    added produtcs

--- frontend/src/app/products/page.js
+++ frontend/src/app/products/page.js
@@ -1,18 +1,19 @@
-import { headers } from "next/headers";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
 
 // This is a server component that fetches the data from the Strapi API
 export default async function Products() {
- let headers = {
+  let headers = {
     Authorization:
       "Bearer e1ba405f90fe3097db8d29d7d3a0dc61798183cb76b469f33ce8b90f22e342d5a3a17135db2c0fe7299d0573eb315069e71675ca26c4dff3e9fe64e136c079632d3a7ce5da527baaad9e25562e7b831e24199728292171c639c7a224b86d6ee6e818d95ff3abda64f1b222a40a8a3b53c25e7a6f8f1917c8fce302be2b42c1ae",
   };
-  let data = await fetch("http://localhost:1337/api/products?populate=*", {headers:headers})
+  let data = await fetch("http://localhost:1337/api/products?populate=*", {
+    headers: headers,
+    cache: "no-store",
+  });
   let products = await data.json();
-  console.log("products" ,products.data.length);
+  // console.log("products", products.data.length, products);
 
-  
   return (
     <div className="container mx-auto">
       <section className="text-gray-600 body-font">
@@ -29,32 +30,42 @@ export default async function Products() {
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
-          {products.data.map((item, index) => (
-  <div key={index} className="xl:w-1/4 md:w-1/2 p-4">
-    <div className="bg-gray-100 p-6 rounded-lg">
-      <Image
-        className="h-40 rounded w-full object-cover object-center mb-6"
-        src={
-          item.attributes.image.data?.attributes?.url
-            ? `http://localhost:1337${item.attributes.image.data.attributes.url}`
-            : "/default-image.jpg"
-        }
-        alt={item.attributes.name || "Product Image"}
-        width={720}
-        height={400}
-      />
-      <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-        {item.attributes.title}
-      </h3>
-      <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-        {item.attributes.name}
-      </h2>
-      <p className="leading-relaxed text-base">
-        {item.attributes.discription || "No description available."}
-      </p>
-    </div>
-  </div>
-))}
+            {products.data.map((item, index) => (
+              <div key={index} className="xl:w-1/4 md:w-1/2 p-4">
+                <div className="bg-gray-100 p-3 rounded-lg">
+                  <Image
+                    className="h-64 rounded w-full object-cover object-center mb-6"
+                    src={
+                      item.attributes.image.data?.attributes?.url
+                        ? `http://localhost:1337${item.attributes.image.data.attributes.url}`
+                        : "/default-image.jpg"
+                    }
+                    // src={item.attributes.image.data && item.attributes.image.data.attributes.name}
+                    alt={item.attributes.name || "Product Image"}
+                    width={720}
+                    height={400}
+                  />
+                  <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
+                    {item.attributes.title}
+                  </h3>
+                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
+                    {item.attributes.name}
+                  </h2>
+                  <div className="hidden bg-[red] bg-[white] bg-[gray] bg-[blue] bg-[black] bg-[purple] bg-[green]"></div>
+                  <button className={"border-2 border-gray-300 ml-1 rounded-full w-6 h-6 focus:outline-none " + `bg-[${item.attributes.color}]`}></button>
+                  <p className="leading-relaxed text-base">
+                    {item.attributes.discription || "No description available."}
+                  </p>
+                  <Link href={`products/${item.id}`}>
+                    <button
+                      className={`ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded mt-2`}
+                    >
+                      Buy now
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
